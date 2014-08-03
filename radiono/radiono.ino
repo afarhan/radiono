7f1b3ac710dfbbf334f164028e591b59b3372053
Eldon Brown <eldonb@ebcon.com>
2014-08-03 12:26:17 -0700

    Update radiono.ino
    Change REV Code to CD

--- radiono/radiono.ino
+++ radiono/radiono.ino
@@ -24,7 +24,7 @@
 
 //#define RADIONO_VERSION "0.4"
 #define RADIONO_VERSION "0.4.erb" // Modifications by: Eldon R. Brown - WA0UWH
-#define INC_REV "CC.13"           // Incremental Rev Code
+#define INC_REV "CD"           // Incremental Rev Code
 
 
 /*
